b528bdd8353123db862c41c09c623d9f72886a56
ThomF <seniorswanky@icloud.com>
2023-03-22 15:33:17 -0600

    setup actually setup

--- AllSpice/Startup.cs
+++ AllSpice/Startup.cs
@@ -33,6 +33,8 @@ public class Startup
 
     services.AddScoped<AccountsRepository>();
     services.AddScoped<AccountService>();
+    services.AddScoped<RecipeRepository>();
+    services.AddScoped<RecipeService>();
   }
 
   private void ConfigureCors(IServiceCollection services)
